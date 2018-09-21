04250efb79e8609c3bc8649e93b490a40f5e8453
Khoi <khoi.nguyen.tdmu@gmail.com>
2018-09-21 15:08:42 +0700

    Them template vao Front-end

--- Front-end/QLNhaSach/src/app/app.module.ts
+++ Front-end/QLNhaSach/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './client/header/header.component';
+import { ContainerComponent } from './client/container/container.component';
+import { FooterComponent } from './client/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
