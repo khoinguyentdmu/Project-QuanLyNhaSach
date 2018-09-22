c765cdb9b7e364d639d9b87c3c5a22e7567a3635
Khoi Nguyen <khoi.nguyen.tdmu@gmail.com>
2018-09-22 09:08:09 +0700

    Tach component client

--- Front-end/QLNhaSach/src/app/app.module.ts
+++ Front-end/QLNhaSach/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './client/header/header.component';
 import { ContainerComponent } from './client/container/container.component';
 import { FooterComponent } from './client/footer/footer.component';
+import { MainNavComponent } from './client/main-nav/main-nav.component';
+import { AsideLeftComponent } from './client/aside-left/aside-left.component';
+import { MainContentComponent } from './client/main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContainerComponent,
-    FooterComponent
+    FooterComponent,
+    MainNavComponent,
+    AsideLeftComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
